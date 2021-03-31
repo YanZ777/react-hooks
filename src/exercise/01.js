85e0c4285913a3d64ea8bda46de5d2dbb15306d2
Yan <yanzhao777@gmail.com>
2021-03-31 00:40:14 -0700

    Complete Exercise 1, Extra Credit 1
    - add prop to component
    - initialize state to prop

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,10 +2,11 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
+import PropTypes from 'prop-types';
 
-function Greeting() {
+function Greeting({initialName}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const [name, setName] = React.useState('');
+  const [name, setName] = React.useState(initialName);
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
@@ -23,8 +24,12 @@ function Greeting() {
   )
 }
 
+Greeting.propTypes = {
+   initialName: PropTypes.string,
+};
+
 function App() {
-  return <Greeting />
+  return <Greeting initialName='Hello' />
 }
 
 export default App
